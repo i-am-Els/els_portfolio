ce7d4e654bffa99d9caa05eec434fa0ea954cba4
Eniola Emmanuel Olawale <eniolaolawale317@gmail.com>
2025-06-14 18:16:07 +0100

    fixed deploy issue trial 1

--- src/app/portfolio/[slug]/layout.tsx
+++ src/app/portfolio/[slug]/layout.tsx
@@ -1,1 +1,14 @@
- 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Portfolio Project | Eniola Olawale',
+  description: 'View my portfolio project details',
+};
+
+export default function PortfolioLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+} 
