92e8a83b86b611ab684ffc4dd325f15df7225b51
Eniola Emmanuel Olawale <eniolaolawale317@gmail.com>
2026-06-17 09:35:57 +0100

    Updated Skill Contents

--- src/data/about.ts
+++ src/data/about.ts
@@ -10,24 +10,24 @@ export const aboutContent = {
   summary: "I work at the crossroads of gameplay and art—where mechanics meet motion. Whether building systems or shaping shaders, I’m learning fast and building with intention. Every line of code and pixel I place moves me closer to the world I imagine.",
   skills: [
     {
+      icon: "cube",
+      title: "3D Modelling",
+      description: "Creating 3D environments, props, characters and other assets in several DCC tools. "
+    },
+    {
       icon: "code-bracket",
       title: "Gameplay Systems (C++ / UE5)",
-      description: "Building core mechanics, input, and interaction logic in engine."
+      description: "Building core mechanics, input, shader and material logic and interaction logic in engine."
     },
     {
       icon: "paint-brush",
-      title: "Shader Programming",
-      description: "Creating stylised, optimised visuals using node-based and code-driven approaches."
-    },
-    {
-      icon: "cube",
-      title: "Procedural System",
-      description: "Designing dynamic, reusable setups for modelling and layout tasks."
+      title: "Visual Storytelling",
+      description: "Bridging the gap between art and technology all towards impactful and performant storytelling experiences."
     },
     {
       icon: "command-line",
-      title: "Tool Development",
-      description: "Creating scripts and utilities that streamline creative workflows."
+      title: "Procedural System",
+      description: "Designing dynamic, reusable setups for modelling and layout tasks."
     }
   ]
 } as const;
